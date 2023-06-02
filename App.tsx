a1fc5c95ca31c9063f5a30a19f8b49e8209f951b
Arthur Silva Dantas <arthur.dantas@estudante.ifms.edu.br>
2023-06-02 00:22:33 -0300

    Fourth commit
    Foco em rotas

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { NativeBaseProvider, StatusBar } from 'native-base'
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
 import { Loading } from './src/components/Loading';
@@ -14,7 +14,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Home /> : <Loading />}
+      { fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
